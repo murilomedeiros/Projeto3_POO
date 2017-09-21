3d31da1e29ee51290c52e4807d20cbaaa428832f
Murilo Medeiros <murilo_medeiros98@hotmail.com>
2017-09-21 00:29:02 -0300

    Arrumando classe Clientes

--- Projeto3_POO/src/java/br/com/fatecpg/poo/Clientes.java
+++ Projeto3_POO/src/java/br/com/fatecpg/poo/Clientes.java
@@ -10,5 +10,60 @@ package br.com.fatecpg.poo;
  * @author muril
  */
 public class Clientes {
+    private String name;
+    private String cpf;
+    private String rg;
+    private String email;
+    private String telefone;
+    private String endereço;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEndereço() {
+        return endereço;
+    }
+
+    public void setEndereço(String endereço) {
+        this.endereço = endereço;
+    }
+    
     
 }
